b837d9a8a30b73da65900ada9665823aa0bd18c2
Alex Whitney <adwhit@fastmail.com>
2019-06-09 18:02:50 +0100

    make print_tweet more prettierer

--- examples/common/mod.rs
+++ examples/common/mod.rs
@@ -11,12 +11,14 @@
 pub extern crate chrono;
 pub extern crate futures;
 pub extern crate tokio;
+pub extern crate yansi;
 
 use egg_mode;
 use std;
 use std::io::{Read, Write};
 
 use self::tokio::runtime::current_thread::block_on_all;
+use self::yansi::Paint;
 
 //This is not an example that can be built with cargo! This is some helper code for the other
 //examples so they can load access keys from the same place.
@@ -120,72 +122,70 @@ impl Config {
 pub fn print_tweet(tweet: &egg_mode::tweet::Tweet) {
     if let Some(ref user) = tweet.user {
         println!(
-            ">> {} (@{}) posted at {}",
-            user.name,
-            user.screen_name,
+            "{} (@{}) posted at {}",
+            Paint::blue(&user.name),
+            Paint::bold(Paint::blue(&user.screen_name)),
             tweet.created_at.with_timezone(&chrono::Local)
         );
     }
 
     if let Some(ref screen_name) = tweet.in_reply_to_screen_name {
-        println!(">> in reply to @{}", screen_name);
+        println!("➜ in reply to @{}", Paint::blue(screen_name));
     }
 
     if let Some(ref status) = tweet.retweeted_status {
-        if let Some(ref user) = status.user {
-            println!(">> Retweeted from {}:", user.name);
-        }
+        println!("{}", Paint::red("Retweet ➜"));
         print_tweet(status);
         return;
     } else {
-        println!("{}", tweet.text);
+        println!("{}", Paint::green(&tweet.text));
     }
 
-    println!(">> via {} ({})", tweet.source.name, tweet.source.url);
+    println!("➜ via {} ({})", tweet.source.name, tweet.source.url);
 
     if let Some(ref place) = tweet.place {
-        println!("--from {}", place.full_name);
+        println!("➜ from: {}", place.full_name);
     }
 
     if let Some(ref status) = tweet.quoted_status {
-        println!(">> Quoting the following status:");
+        println!("{}", Paint::red("➜ Quoting the following status:"));
         print_tweet(status);
     }
 
     if !tweet.entities.hashtags.is_empty() {
-        println!(">> Hashtags contained in the tweet:");
+        println!("➜ Hashtags contained in the tweet:");
         for tag in &tweet.entities.hashtags {
-            println!("{}", tag.text);
+            println!("  {}", tag.text);
         }
     }
 
     if !tweet.entities.symbols.is_empty() {
-        println!(">> Symbols contained in the tweet:");
+        println!("➜ Symbols contained in the tweet:");
         for tag in &tweet.entities.symbols {
-            println!("{}", tag.text);
+            println!("  {}", tag.text);
         }
     }
 
     if !tweet.entities.urls.is_empty() {
-        println!(">> URLs contained in the tweet:");
+        println!("➜ URLs contained in the tweet:");
         for url in &tweet.entities.urls {
             if let Some(expanded_url) = &url.expanded_url {
-                println!("{}", expanded_url);
+                println!("  {}", expanded_url);
             }
         }
     }
 
     if !tweet.entities.user_mentions.is_empty() {
-        println!(">> Users mentioned in the tweet:");
+        println!("➜ Users mentioned in the tweet:");
         for user in &tweet.entities.user_mentions {
-            println!("{}", user.screen_name);
+            println!("  {}", Paint::bold(Paint::blue(&user.screen_name)));
         }
     }
 
     if let Some(ref media) = tweet.extended_entities {
-        println!(">> Media attached to the tweet:");
+        println!("➜ Media attached to the tweet:");
         for info in &media.media {
-            println!("A {:?}", info.media_type);
+            println!("  A {:?}", info.media_type);
         }
     }
 }
