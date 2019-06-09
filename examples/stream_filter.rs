a1d5d255a53e15f4172ff1418e1ffc1904b8240a
Alex Whitney <adwhit@fastmail.com>
2019-06-09 10:52:02 +0100

    stream: use vec instead of btreemap

--- examples/stream_filter.rs
+++ examples/stream_filter.rs
@@ -9,19 +9,18 @@ use egg_mode::stream::StreamMessage;
 
 fn main() {
     let config = common::Config::load();
-    println!("Streaming tweets containing popular programming languages (also Rust)");
+    println!("Streaming tweets containing popular programming languages (and also Rust)");
     println!("Ctrl-C to quit\n");
 
     let stream = egg_mode::stream::filter()
         .track(&["rustlang", "python", "java", "javascript"])
         .start(&config.token)
         .for_each(|m| {
-            match m {
-                StreamMessage::Tweet(tweet) => {
-                    common::print_tweet(&tweet);
-                    println!();
-                }
-                other => println!("{:?}", other),
+            if let StreamMessage::Tweet(tweet) = m {
+                common::print_tweet(&tweet);
+                println!();
+            } else {
+                println!("{:?}", m);
             }
             futures::future::ok(())
         });
--- src/stream/mod.rs
+++ src/stream/mod.rs
@@ -4,7 +4,7 @@
 
 //! Access to the Streaming API.
 
-use std::collections::{BTreeSet, HashMap};
+use std::collections::HashMap;
 use std::str::FromStr;
 use std::{self, io};
 
@@ -289,8 +289,8 @@ impl ::std::fmt::Display for FilterLevel {
 /// Represents a `TwitterStream` before it is started.
 pub struct StreamBuilder {
     url: &'static str,
-    follow: BTreeSet<u64>,
-    track: BTreeSet<String>,
+    follow: Vec<u64>,
+    track: Vec<String>,
     filter_level: Option<FilterLevel>,
 }
 
@@ -298,23 +298,24 @@ impl StreamBuilder {
     fn new(url: &'static str) -> Self {
         StreamBuilder {
             url: url,
-            follow: BTreeSet::new(),
-            track: BTreeSet::new(),
+            follow: Vec::new(),
+            track: Vec::new(),
             filter_level: None,
         }
     }
 
     /// List of user IDs indicating the users whose Tweets should be delivered on the stream
     pub fn follow(mut self, to_follow: &[u64]) -> Self {
-        self.follow.extend(to_follow.iter());
+        self.follow.extend(to_follow.into_iter());
         self
     }
 
     /// List of phrases which will be used to determine what Tweets will be delivered on the stream.
     /// A phrase may be one or more terms separated by spaces, and a phrase will match if all
     /// of the terms in the phrase are present in the Tweet, regardless of order and ignoring case.
-    pub fn track(mut self, to_track: &[&str]) -> Self {
-        self.track.extend(to_track.iter().map(|s| s.to_string()));
+    pub fn track<I: IntoIterator<Item = S>, S: AsRef<str>>(mut self, to_track: I) -> Self {
+        self.track
+            .extend(to_track.into_iter().map(|s| s.as_ref().to_string()));
         self
     }
 
@@ -350,12 +351,7 @@ impl StreamBuilder {
         }
 
         if !self.track.is_empty() {
-            let to_track = self
-                .track
-                .iter()
-                .map(String::as_str)
-                .collect::<Vec<&str>>()
-                .join(",");
+            let to_track = self.track.join(",");
             add_param(&mut params, "track", to_track);
         }
 
