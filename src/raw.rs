364e4a853e86118b7b3bd1a21c4cb23f8de1656a
QuietMisdreavus <victoria@quietmisdreavus.net>
2020-06-03 10:38:31 -0600

    expand `raw` module docs

--- src/raw.rs
+++ src/raw.rs
@@ -14,25 +14,54 @@
 //! executing it to get a response. Some wrapper types in egg-mode operate directly on requests, or
 //! create their own, so constructors for those types are also exposed here.
 //!
-//! The functions that create `Request`s (or that assemble a type that creates its own `Requests`)
-//! all require a `Token`, like the rest of egg-mode, which lets them properly create the
-//! corresponding OAuth signature for the call. They also take a `ParamList` instance, which is
-//! used to store parameters to the API call. These correspond to the parameters listed on the API
-//! Reference page for the given endpoint you would like to call.
+//! To start using the functions in this module, you'll need a [`Token`] from the authentication
+//! flow. The parameters to an endpoint are represented by the [`ParamList`] type. They're
+//! separated out so that they can be included as part of the OAuth signature given to Twitter as
+//! part of the API call. This also means that the URL you give to the request functions should be
+//! the base URL, with no parameters.
+//!
+//! [`Token`]: ../enum.Token.html
+//! [`ParamList`]: struct.ParamList.html
+//!
+//! There are three basic request functions, based on how the endpoint expects to be called:
+//!
+//! * `request_get` assembles a GET request, with the given parameters appended to the URL as a
+//!   query string. All GET endpoints that egg-mode currently wraps use this function to encode and
+//!   sign the request.
+//! * `request_post` assembles a POST request, with the given parameters included in the POST body
+//!   formatted as `x-www-form-urlencoded` data. Most POST endpoints in the Twitter API are
+//!   formatted using this function.
+//! * `request_post_json` also assembles a POST request, but instead of taking a `ParamList`, it
+//!   takes arbitrary data and formats it in the POST body as JSON. The provided data is *not* used
+//!   as part of the OAuth signature. At time of writing (between releases 0.14 and 0.15) the only
+//!   egg-mode endpoint that uses this function is [`media::set_metadata`].
+//!
+//! [`media::set_metadata`]: ../media/fn.set_metadata.html
 //!
 //! Once you have a `Request`, you can hand it to the `response_*` functions in this module to
 //! process it. Which one you select depends on how much processing you want egg-mode to do with
-//! the response.
+//! the response:
 //!
-//! * At the most hands-off end, there's `response_future`, which is a small wrapper that just
+//! * At the most hands-off end, there's [`response_future`,] which is a small wrapper that just
 //!   starts the request and hands off the `ResponseFuture` from `hyper` to give you the most power
 //!   over handling the response data.
-//! * In the middle, there's `response_raw_bytes`, which wraps the `ResponseFuture` to return the
+//! * In the middle, there's [`response_raw_bytes`], which wraps the `ResponseFuture` to return the
 //!   headers and response body after inspecting the rate-limit headers and response code, and
 //!   after inspecting the response to see whether it returned error data from Twitter.
-//! * Finally there's `response_json`, which picks up from `response_raw_bytes` to parse the
+//! * Finally there's [`response_json`], which picks up from `response_raw_bytes` to parse the
 //!   response as JSON and deserialize it into the target type, alongside the rate-limit
 //!   information from the response headers.
+//!
+//! [`response_future`]: fn.response_future.html
+//! [`response_raw_bytes`]: fn.response_raw_bytes.html
+//! [`response_json`]: fn.response_json.html
+//!
+//! In addition, there are `request_as_*` and `response_as_*` functions available to format a
+//! request using one of the wrappers used in egg-mode. If the endpoint you're using is one that
+//! currently uses one of these wrapper types or returns and accepts data the same way as one of
+//! these endpoints, you can use these functions to get the same experience as the existing
+//! wrappers in egg-mode. See the documentation for these functions to see their assumptions and
+//! requirements.
 
 use hyper::{Body, Request};
 
