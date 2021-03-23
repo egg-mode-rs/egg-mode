2a8d965a8c64f438c94f7e84b25d10a3439b5c97
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-03-24 01:33:41 +0900

    add comment

--- src/account/mod.rs
+++ src/account/mod.rs
@@ -1,59 +1,50 @@
-use crate::common::ParamList;
+use crate::{
+    auth,
+    common::{post, request_with_empty_response, request_with_json_response, ParamList},
+    error, links,
+    user::TwitterUser,
+    Response,
+};
 
-use crate::common::*;
-use crate::{auth, error, links, user::TwitterUser};
-
-use serde::{Deserialize, Serialize};
-
-/// TODO
+/// Parameters  
+/// account/update_profile_banner
 pub struct ProfileBannerOption {
+    /// The width of the preferred section of the image being uploaded in pixels.
+    /// Use with height , offset_left , and offset_top to select the desired region of the image to use.
     pub width: Option<String>,
+    /// The height of the preferred section of the image being uploaded in pixels.
+    /// Use with width , offset_left , and offset_top to select the desired region of the image to use.
     pub height: Option<String>,
+    /// The number of pixels by which to offset the uploaded image from the left.
+    /// Use with height , width , and offset_top to select the desired region of the image to use.
     pub offset_left: Option<String>,
+    /// The number of pixels by which to offset the uploaded image from the top.
+    /// Use with height , width , and offset_left to select the desired region of the image to use.
     pub offset_top: Option<String>,
 }
 
-/// TODO
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ProfileBanner {
-    sizes: Sizes,
-}
-
-/// TODO
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Sizes {
-    ipad: ImageSize,
-    ipad_retina: ImageSize,
-    web: ImageSize,
-    web_retina: ImageSize,
-    mobile: ImageSize,
-    mobile_retina: ImageSize,
-    #[serde(rename = "300x100")]
-    s300x100: ImageSize,
-    #[serde(rename = "600x200")]
-    s600x200: ImageSize,
-    #[serde(rename = "1500x500")]
-    s1500x500: ImageSize,
-}
-
-/// TODO
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ImageSize {
-    pub h: String,
-    pub w: String,
-    pub url: String,
-}
-
-/// TODO
+/// Parameters
+/// account/update_profile  
 pub struct UserProfile {
+    /// Full name associated with the profile.
     pub name: Option<String>,
+    /// URL associated with the profile. Will be prepended with http:// if not present.
     pub url: Option<String>,
+    /// The city or country describing where the user of the account is located. The contents are not normalized or geocoded in any way.
     pub location: Option<String>,
+    /// A description of the user owning the account.
     pub description: Option<String>,
+    /// Sets a hex value that controls the color scheme of links used on the authenticating user's profile page on twitter.com.
+    /// This must be a valid hexadecimal value, and may be either three or six characters (ex: F00 or FF0000).
+    /// This parameter replaces the deprecated (and separate) update_profile_colors API method.
     pub profile_link_color: Option<String>,
 }
 
-/// TODO
+/// Updates the authenticating user's profile image.
+/// Note that this method expects raw multipart data, not a URL to an image.
+///
+/// This method asynchronously processes the uploaded file before updating the user's profile image URL.
+/// You can either update your local cache the next time you request the user's information, or, at least 5 seconds after uploading the image, ask for the updated URL using GET users / show.
 pub async fn update_profile_image(
     image: &[u8],
     token: &auth::Token,
@@ -63,12 +54,16 @@ pub async fn update_profile_image(
     request_with_json_response(req).await
 }
 
-/// TODO
+/// Uploads a profile banner on behalf of the authenticating user.
+/// More information about sizing variations can be found in User Profile Images and Banners and GET users / profile_banner.
+///
+/// Profile banner images are processed asynchronously.
+/// The profile_banner_url and its variant sizes will not necessary be available directly after upload.
 pub async fn update_profile_banner(
     banner: &[u8],
     options: Option<ProfileBannerOption>,
     token: &auth::Token,
-) -> error::Result<Response<ProfileBanner>> {
+) -> error::Result<Response<()>> {
     let params = match options {
         Some(o) => ParamList::new()
             .add_param("banner", base64::encode(banner))
@@ -81,10 +76,11 @@ pub async fn update_profile_banner(
 
     let req = post(links::account::UPDATE_PROFILE_BNNER, token, Some(&params));
 
-    request_with_json_response(req).await
+    request_with_empty_response(req).await
 }
 
-/// TODO
+/// Sets some values that users are able to set under the "Account" tab of their settings page.
+/// Only the parameters specified will be updated.
 pub async fn update_profile(
     user_profile: UserProfile,
     token: &auth::Token,
